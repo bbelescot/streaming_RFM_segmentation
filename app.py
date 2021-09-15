839eab09f828cedc253bcf188ffbc08d7166de92
Baptiste Belescot <47362338+bbelescot@users.noreply.github.com>
2021-09-15 10:34:18 +0200

    change title

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.title("A quick yet powerful user segmentation using RFM")
+st.title("Powerful user segmentation using RFM")
 
 
 '''
