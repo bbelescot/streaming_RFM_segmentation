d4a1489cb6663c23f1594f31efcf11cfb63fc4e6
Baptiste Belescot <47362338+bbelescot@users.noreply.github.com>
2021-09-15 10:32:38 +0200

    typo

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.title("A quick yet powerful user segmentation using RFM" )
+st.title("A quick yet powerful user segmentation using RFM")
+
 
 '''
 
